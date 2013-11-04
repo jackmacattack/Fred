bfc5db8bff5ee9c9884c50212fba6dc711ee1cbb
Jack McNamara <jackmacattack92@gmail.com>
2013-11-04 08:12:36 -0500

    Updated client unit test

--- client.py
+++ client.py
@@ -1,7 +1,33 @@
 __author__ = 'Jack'
 
+import socket               # Import socket module
 
 class Client:
 
+	def __init__(self):
+		host = "10.0.0.18"#socket.gethostname() # Get local machine name
+		port = 12344                # Reserve a port for your service.
+
 	def upload(self, file_name):
-		pass
+		s = socket.socket()         # Create a socket object
+
+		s.connect((host, port))     # print recieved data
+
+		s.send(f)
+		readByte=open(file_name,"rb")       #read file
+		data = readByte.read()
+		readByte.close()
+
+		s.send(data)
+		s.close                     # Close the socket when done
+		
+	def send_message(self, message): 
+		s = socket.socket()         # Create a socket object
+
+		s.connect((host, port))     # print recieved data
+
+		s.send(message)
+
+		s.close                     # Close the socket when done
+		
+
---
+++ test_client.py
@@ -0,0 +1,60 @@
+__author__ = "Jack"
+
+import client, unittest, socket, thread, time
+
+
+class TestClient(unittest.TestCase):
+
+    server_test = False
+
+    def server(self):
+        s = socket.socket()         # Create a socket object
+        host = socket.gethostname() # Get local machine name
+        port = 12345                # Reserve a port for your service.
+
+        s.bind((host, port))        # Bind to the port
+
+        s.listen(5)                 # Now wait for client connection.
+
+        for i in range(10000):
+            c, addr = s.accept()     # Establish connection with client.
+
+            name = c.recv(1024)     # create the file
+            print name
+            if name[-4:] == ".dat":
+                name = c.recv(1024)
+
+            if name == "Love":
+                self.server_test = True
+                thread.exit()
+
+            c.close()                # Close the connection
+
+    def setUp(self):    #Sets up a test server
+        server_test = False
+
+        thread.start_new_thread(self.server, ())
+
+    def test_message(self):
+        c = client.Client()
+        c.send_message("Love")
+
+        time.sleep(5)
+
+        self.assertTrue(self.server_test)
+
+    def test_upload(self):
+        file = open("testing_temp_123456789876543213531.dat","w")
+        file.write("Love")
+        file.close()
+
+        c = client.Client()
+        c.upload("testing_temp_123456789876543213531.dat")
+
+        time.sleep(5)
+
+        self.assertTrue(self.server_test)
+
+    def tearDown(self):
+        pass
+        #thread.exit()
