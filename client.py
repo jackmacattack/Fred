b09de88826e844fafbcee3d688fa04f5e6c9afde
mcs8sc <mcs8sc@virginia.edu>
2013-11-17 15:29:44 -0500

    Final changes to interface for client.

--- client.py
+++ client.py
@@ -9,7 +9,7 @@ class Client:
     def send_credentials(self, username, password) :
         pass
 
-    def create_username(self, new_username) :
+    def create_account( new_username, new_password, password_question, password_answer ) :
         pass
 
     def forgotten_password_check(self, username, answer ):
